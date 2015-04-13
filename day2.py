c5fd1de0f708f60a826bf1da42d9f82597b90d9e
catenotkate <caterpillar0714@gmail.com>
2015-04-13 12:22:36 -0700

    Update day2.py
    Problemo
    
    Traceback (most recent call last):
      File "day_2.py", line 30, in <module>
        f.write (savestuff.join ("\n"))
    AttributeError: 'list' object has no attribute 'join'
    
    whatever that means

--- day2.py
+++ day2.py
@@ -1,4 +1,8 @@
 import random
+
+f = open ('answers.csv', 'w')
+savestuff = []
+
 def awesome():
  print ("Cool! See you tomorrow")
 
@@ -8,22 +12,30 @@ def okay ():
 def horrible ():
  print ("That's too bad... hope tomorrow is better")
 
-
 def ask ():
     q=["How was your day?  ", "How was your coffee?  ", "How was your date?  "]
     return q[random.randint(0,2)]
 
-for i in range(10):
+while 1 == 1:
     x = raw_input(ask())
     print (x)
-    if x == 'okay' or x == 'fine':
+    if x == 'okay' or x == 'fine' or x == 'meh' or x == 'nothing special':
         okay()
-    elif x == 'awesome' or x == 'good' or x == 'great':
+    elif x == 'awesome' or x == 'good' or x == 'great' or x == 'wonderful' or x == 'amazing' or x == 'really good':
         awesome()
-    elif x =='horrible' or x == 'bad' or x == 'pretty bad':
+    elif x =='horrible' or x == 'bad' or x == 'pretty bad' or x == 'boring':
         horrible()
+    elif x == 'how was yours?':
+        print savestuff
+        f.write (savestuff.join ("\n"))
+        f.write ("")
     else:
         print ("Yeah, same here... well, I'm off to Starbucks")
+    savestuff.append(x)
+
+f.close ()
+
+
 
 
 
